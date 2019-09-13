32f1f117bfa6c01511763bd593b3e416e86b91fe
Marcus Trujillo <marcus.t.trujillo@gmail.com>
2019-09-12 21:45:15 -0600

    straightened out my iml business

--- src/ConsoleDisplay.java
+++ src/ConsoleDisplay.java
@@ -11,7 +11,7 @@ public class ConsoleDisplay {
         if(board.isEmpty()){ System.out.println("Empty Board "); }
         System.out.println("***BOARD****");
         System.out.println(board);
-        System.out.println("*******\n");
+        System.out.println("*******");
     }
 
     public void shiftBoard(){
